bcd8065da764a18f51cff4c8512c136e8607aae4
Neenu Ann Varghese <neenuavarghese@gmail.com>
2017-02-05 11:20:43 -0800

    Removeda bug - Remove specific elemnt from array

--- RemoveElement.java
+++ RemoveElement.java
@@ -6,7 +6,7 @@ Do not allocate extra space for another array, you must do this in place with co
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int j = 0;
-        for(int i = 0; i+1 <nums.length; i++){
+        for(int i = 0; i<nums.length; i++){
             if(nums[i] != val ){
                 nums[j] = nums[i];
                 j++;
