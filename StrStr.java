5650fcdfbad4faf2e259e8660cce3df1202ed5e6
Neenu Ann Varghese <neenuavarghese@gmail.com>
2017-02-05 11:20:18 -0800

    Implementation of strStr() in java

---
+++ StrStr.java
@@ -0,0 +1,39 @@
+/*
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        if(haystack==null || needle==null){
+            return 0;
+        }
+        int hayLen = haystack.length();
+        int needleLen = needle.length();
+        
+        if(hayLen < needleLen){
+            return -1;
+        }
+
+        if(needleLen == 0){
+            return 0;
+        }
+        for(int i = 0;i <=hayLen - needleLen;i++){
+           
+            if(haystack.charAt(i) == needle.charAt(0)){
+               
+                int j = 0;
+                for(; j < needleLen; j++){
+                    if(haystack.charAt(i+j) != needle.charAt(j)){
+                        break;
+                    }
+                }
+                
+                if(j == needleLen){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
