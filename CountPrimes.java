bd37b72491525c944c223775f16c1476b81935f9
Neenu Ann Varghese <neenuavarghese@gmail.com>
2017-02-18 22:46:17 -0800

    Counts total prime numbers upto a givenn number

---
+++ CountPrimes.java
@@ -0,0 +1,28 @@
+/*
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+Hint:
+Let's start with a isPrime function. To determine if a number is prime, we need to check if it is not divisible by any number less than n. The runtime complexity of isPrime function would be O(n) and hence counting the total prime numbers up to n would be O(n2). Could we do better?
+*/
+
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] m = new boolean[n];
+        
+          for(int i=2;i<n;i++){
+            if(m[i]){
+                continue;
+            }  
+            count++;
+            
+            for(int j=i;j<n;j=j+i){
+                m[j]= true;
+            }
+          }
+          
+          return count;
+    }
+}
