0bd783e73e6f1742f0f9fd7df8cddb90ae4fcac2
Neenu Ann Varghese <neenuavarghese@gmail.com>
2017-07-04 15:38:56 -0700

    Rising Temperatures - SSQL

---
+++ Rising Temperature.sql
@@ -0,0 +1,23 @@
+Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------+------------------+
+| Id(INT) | Date(DATE) | Temperature(INT) |
++---------+------------+------------------+
+|       1 | 2015-01-01 |               10 |
+|       2 | 2015-01-02 |               25 |
+|       3 | 2015-01-03 |               20 |
+|       4 | 2015-01-04 |               30 |
++---------+------------+------------------+
+For example, return the following Ids for the above Weather table:
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+
+
+# Write your MySQL query statement below
+
+select w2.id from weather as w1 , weather as w2
+where w1.temperature < w2.temperature and TO_DAYS(w2.DATE) - TO_DAYS(w1.DATE) = 1; 
